a7aba47d6b1bd5519d18d2e68dda8867f5fcf81f
wasafairy <sskp22022@gmail.com>
2024-12-02 11:04:09 +0530

    Add files via upload

---
+++ pat3-1.py
@@ -0,0 +1,19 @@
+# PAT-TASK 3
+#PROGRAM 1
+# function for odd_even
+def odd_even(list_a):
+    even_list = []
+    odd_list = []
+    for i in list_a:
+        if (i % 2 == 0):
+            even_list.append(i)
+        else:
+            odd_list.append(i)
+
+    print(even_list)
+    print(odd_list)
+
+
+# main program
+list_a = [10, 501, 22, 37, 100, 999, 87, 35]
+print(odd_even(list_a))
