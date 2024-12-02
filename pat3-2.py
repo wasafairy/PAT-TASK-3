5e75351c39837c8d998d92b1e565d2e1996a5f83
wasafairy <sskp22022@gmail.com>
2024-12-02 11:05:59 +0530

    Add files via upload

---
+++ pat3-2.py
@@ -0,0 +1,23 @@
+#program 2
+numberList = [10,501,22,37,100,999,87,35]
+ansList = []
+for num in numberList :
+    
+    if num == 0 or num == 1 :
+        continue
+        
+    for i in range(2, num // 2 + 1) :
+ 
+        if num % i == 0 :
+            break
+ 
+    else :
+         ansList.append(num)
+ 
+if len(ansList) :
+     
+    for ans in ansList :
+        print([ans])
+     
+else :
+    print("No any number from the given list is Prime")
